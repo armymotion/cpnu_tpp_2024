6c0dbf052268df87f2b0fd4cb96e6b6d47a3cfe7
sayori1418 <sas717818@gmail.com>
2024-09-19 09:39:32 +0300

    Update main.js

--- KI-222/sayori1418/lab1/main.js
+++ KI-222/sayori1418/lab1/main.js
@@ -14,7 +14,7 @@
 <section>
     <div class="profile">
         <img src="images/DR0laxvHchT8pKn3pArcycQH1GeGnjIXRRFWYyDsgwPhV-yRM_5ssLpWNrag1vXNWS0jfcoThrAUV92hvXuZnNPbganoAvYDxxX3QLhrR8SRBLoxxigRpe34tETWeKARX3F_unfl39aTyoD6593V-u6d1GvAKSJfBkCB7NLtkGDAwl8G9FmNsbeCbYcR5hWyllffO5N4ya5PthluwHVcD85.jpg" alt="Profile Picture" class="profile-pic">
-            <p>Hi, I'm Ser Mak, a student with a passion to become a great IT specialist. Welcome to KI-000.</p>
+            <p>Hi, I'm Ser Mak, a student with a passion to become a great IT specialist. Welcome to KI-222.</p>
     </div>
 </section>
 
